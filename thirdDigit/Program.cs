79460aeb17b8c1b98f280591a56367317060c84c
Дмитрий <dimon20.07@mail.ru>
2022-09-05 22:53:32 +0300

    thirdDigit

---
+++ thirdDigit/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей 
+//цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("enter the number: ");
+string? number = Console.ReadLine();
+
+if (number?.Length > 2)
+{
+  Console.WriteLine($"Third digit " + number[2]);
+}
+else
+{
+  Console.WriteLine("Третьей цифры нет");
+}
